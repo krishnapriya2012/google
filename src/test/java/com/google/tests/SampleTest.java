440b5471aecce73946bebbbd3e394b4845cf00bb
krishna2012 <nkpriya91@gmail.com>
2018-05-01 16:46:44 +0530

    nkp again commit

--- src/test/java/com/google/tests/SampleTest.java
+++ src/test/java/com/google/tests/SampleTest.java
@@ -9,10 +9,10 @@ public class SampleTest {
 	@Test
 	public void testSample()
 	{
-		String browser=System.getProperty("browser");
+		/*String browser=System.getProperty("browser");
 		String url=System.getProperty("url");
 		System.out.println(browser);
-		System.out.println(url);
+		System.out.println(url);*/
 		System.out.println("helloo success");
 		
 	}
